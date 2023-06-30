0122efab28818c7fbcd0ec16a6cef2fe120946dc
Nikita Malinovsky <nikita@malinovsky.net>
2023-06-29 21:13:45 -0700

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from "next/app";
 import SeoLayout from "@/components/Layouts/seo";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme/styles";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <SeoLayout>
         <Component {...pageProps} />
+        <Analytics />
       </SeoLayout>
     </ChakraProvider>
   );
